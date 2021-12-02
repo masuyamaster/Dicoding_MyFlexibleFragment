3e717184784e7e64e0e7b8a6e03869e1cb95460b
masuyamaster <roziq@masuya.id>
2021-12-02 20:41:54 +0700

    latihan activity KTX dan fragment KTX

--- app/src/main/java/com/example/myflexiblefragment/HomeFragment.kt
+++ app/src/main/java/com/example/myflexiblefragment/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.commit
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -47,10 +48,9 @@ class HomeFragment : Fragment(), View.OnClickListener {
         if (v.id == R.id.btn_category) {
             val mCategoryFragment = CategoryFragment()
             val mFragmentManager = parentFragmentManager
-            mFragmentManager.beginTransaction().apply {
-                replace(R.id.frame_container, mCategoryFragment, CategoryFragment::class.java.simpleName)
+            mFragmentManager.commit {
                 addToBackStack(null)
-                commit()
+                replace(R.id.frame_container, mCategoryFragment, CategoryFragment::class.java.simpleName)
             }
         }
 
